4793aef17e5b0f453532c08c981e6a6b64ddc9af
tirawat.saijai <tirawat.saijai@gmail.com>
2024-09-24 10:49:17 +0700

    feat: add api users mock up

--- components/auth/GoogleSignIn.vue
+++ components/auth/GoogleSignIn.vue
@@ -2,28 +2,42 @@
 const box = ref();
 
 onMounted(() => {
-  GoogleLogin();
+  initGoogleAuth();
 });
-const GoogleLogin = async () => {
-  // js google login
+
+const initGoogleAuth = async () => {
   google.accounts.id.initialize({
     client_id:
       "130124382359-n9qk0adj7q46rern65lkpj378g464ctf.apps.googleusercontent.com",
     callback: handleCredentialResponse,
+    auto_select: false  
   });
+
   google.accounts.id.renderButton(box.value, {
     type: "standard",
     theme: "filled_blue",
-    size: "large"
+    size: "large",
   });
+
 };
 
-const handleCredentialResponse = async (response) => {
+const onSignout = () => {
+  google.accounts.id.disableAutoSelect();
+  localStorage.removeItem('google_token');
+  window.location.reload()
+};
+
+const handleCredentialResponse = async (response: Object) => {
   console.log(response);
 };
 </script>
 <template>
   <div>
     <div ref="box" class="d-flex justify-content-center"></div>
+    <div>
+      <button class="btn btn-info btn-sm" @click="onSignout">
+        Sign-out
+      </button>
+    </div>
   </div>
 </template>
